0a4647b3b2ca2590f730724f37eeee79a5186fde
Sakibul-05 <110473516+Sakibul-05@users.noreply.github.com>
2023-12-15 00:52:42 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,71 @@
 //your JS code here. If required.
+const addButton = document.querySelector("#add-button");
+const clearbutton = document.querySelector("#clear-button");
+const toasts = document.querySelector("#toasts");
+
+function addToasts(message){
+	const div = document.createElement("div");
+	div.classList.add("toast", "success-toast");
+	const p = document.createElement("p");
+	p.classList.add("message");
+	p.innerText = message;
+	const button = document.createElement("button");
+	button.innerText = "X";
+	div.append(p, button);
+	return div;
+}
+addButton.addEventListener("click", (event)=>{
+	toasts.append(addToasts("Hello..."));
+})
+clearbutton.addEventListener("click", (event)=>{
+	toasts.innerHTML = "";
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
